aec0365d01a1e794464fe79154228de87068f139
Karol Wójcik <karol.wojcik@tuta.io>
2021-04-19 09:52:26 +0200

    [deployer] Release v0.1.15

--- examples/sqs-example/project.clj
+++ examples/sqs-example/project.clj
@@ -1,6 +1,6 @@
 (defproject sqs-example "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure         "1.10.0"]
-                 [io.github.FieryCod/holy-lambda   "0.1.13"]
+                 [io.github.FieryCod/holy-lambda   "0.1.15"]
                  [com.cognitect.aws/api       "0.8.484"]
                  [com.cognitect.aws/endpoints "1.1.11.926"]
                  [com.cognitect.aws/sqs       "810.2.817.0"]]
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.github.FieryCod/holy-lambda   "0.1.14-SNAPSHOT"
+(defproject io.github.FieryCod/holy-lambda   "0.1.15"
   :description "Micro framework which turns your code into AWS Lambda functions"
 
   :url "https://github.com/FieryCod/holy-lambda"
