ec32241a9a1c880597bc6702087eddd04f6c5919
Karol Wójcik <karol.wojcik@tuta.io>
2021-04-19 10:24:22 +0200

    [deployer] Release v0.1.18

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.github.FieryCod/holy-lambda   "0.1.17-SNAPSHOT"
+(defproject io.github.FieryCod/holy-lambda   "0.1.18"
   :description "Micro framework which turns your code into AWS Lambda functions"
 
   :url "https://github.com/FieryCod/holy-lambda"
