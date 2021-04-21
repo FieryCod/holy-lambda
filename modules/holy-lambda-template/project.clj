dd16a110274fa600d9ca287ea3d8ad63aebe9eed
Karol Wójcik <karol.wojcik@tuta.io>
2021-04-21 21:18:04 +0200

    [deployer] Prepare for next development iteration v0.1.26-SNAPSHOT

--- modules/holy-lambda-template/project.clj
+++ modules/holy-lambda-template/project.clj
@@ -1,4 +1,4 @@
-(defproject holy-lambda/lein-template   "0.1.25"
+(defproject holy-lambda/lein-template   "0.1.26-SNAPSHOT"
   :description "Template for holy lambda micro framework"
 
   :url "https://github.com/FieryCod/holy-lambda/tree/master/packages/holy-lambda-template"
