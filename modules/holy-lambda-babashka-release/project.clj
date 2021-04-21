5604a81e8ae4c1aa8af664b54fcef9d3df99deca
Karol Wójcik <karol.wojcik@tuta.io>
2021-04-21 23:12:09 +0200

    [deployer] Release v0.1.30

--- modules/holy-lambda-babashka-release/project.clj
+++ modules/holy-lambda-babashka-release/project.clj
@@ -1,4 +1,4 @@
-(defproject io.github.FieryCod/holy-lambda-babashka   "0.1.30-SNAPSHOT"
+(defproject io.github.FieryCod/holy-lambda-babashka   "0.1.30"
   :description "Micro framework which turns your code into AWS Lambda functions suites for babashka based Lambdas"
 
   :url "https://github.com/FieryCod/holy-lambda/tree/master/modules/holy-lambda-babashka"
