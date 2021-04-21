c4435ffcbe2908619952bcdb5fba22b802695854
Karol Wójcik <karol.wojcik@tuta.io>
2021-04-21 11:37:30 +0200

    [deployer] Prepare for next development iteration v0.1.20-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.github.FieryCod/holy-lambda   "0.1.19"
+(defproject io.github.FieryCod/holy-lambda   "0.1.20-SNAPSHOT"
   :description "Micro framework which turns your code into AWS Lambda functions"
 
   :url "https://github.com/FieryCod/holy-lambda"
