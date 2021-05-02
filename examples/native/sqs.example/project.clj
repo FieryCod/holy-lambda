19b92cda87727f0f98ee9b87d46d47c3a7314c7f
Karol Wójcik <karol.wojcik@tuta.io>
2021-05-02 10:30:54 +0200

    Retry native-conf fix

--- examples/native/sqs.example/project.clj
+++ examples/native/sqs.example/project.clj
@@ -1,11 +1,11 @@
-(defproject sqs-example "0.1.0-SNAPSHOT"
+(defproject sqs.example  "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure         "1.10.3"]
                  [io.github.FieryCod/holy-lambda   "0.1.44"]
                  [com.cognitect.aws/api       "0.8.505"]
                  [com.cognitect.aws/endpoints "1.1.11.1001"]
                  [com.cognitect.aws/sqs       "811.2.889.0"]]
   :global-vars {*warn-on-reflection* true}
-  :main ^:skip-aot sqs-example.core
+  :main ^:skip-aot sqs.example.core
   :profiles {:uberjar {:aot      :all
                        :jvm-opts ["-Dclojure.compiler.direct-linking=true"
                                   "-Dclojure.spec.skip-macros=true"]}}
