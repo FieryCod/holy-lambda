0476f8c14fe440642d36b2a5d42b5b9f9e88d833
Karol Wójcik <karol.wojcik@tuta.io>
2021-05-03 10:25:30 +0200

    Bump tasks sha of tasks for all examples

--- modules/holy-lambda-template/project.clj
+++ modules/holy-lambda-template/project.clj
@@ -1,4 +1,4 @@
-(defproject holy-lambda/lein-template   "0.1.46"
+(defproject holy-lambda/lein-template   "0.1.47"
   :description "Template for holy lambda micro framework"
 
   :url "https://github.com/FieryCod/holy-lambda/tree/master/packages/holy-lambda-template"
