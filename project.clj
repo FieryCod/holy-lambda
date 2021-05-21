5177a93ef31c4f5b1ff25d1327d9ccf96b132e2f
Karol Wójcik <karol.wojcik@tuta.io>
2021-05-21 16:38:48 +0200

    Update holy-lambda-version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.github.FieryCod/holy-lambda   "0.1.48"
+(defproject io.github.FieryCod/holy-lambda   "0.1.49"
   :description "Micro framework which turns your code into AWS Lambda functions"
 
   :url "https://github.com/FieryCod/holy-lambda"
