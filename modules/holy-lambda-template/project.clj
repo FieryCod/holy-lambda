6e6409d0827a92e4cdf56a3fa7357dc41defb875
Karol Wójcik <karol.wojcik@tuta.io>
2021-05-21 16:36:38 +0200

    Bump template

--- modules/holy-lambda-template/project.clj
+++ modules/holy-lambda-template/project.clj
@@ -1,4 +1,4 @@
-(defproject holy-lambda/lein-template   "0.1.56"
+(defproject holy-lambda/lein-template   "0.1.57"
   :description "Template for holy lambda micro framework"
 
   :url "https://github.com/FieryCod/holy-lambda/tree/master/packages/holy-lambda-template"
