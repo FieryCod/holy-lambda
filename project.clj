44db55bea02e48909a34c84c0aac28aef91f596d
Karol Wójcik <karol.wojcik@tuta.io>
2021-06-06 16:29:01 +0200

    Ignore template-modified.yml

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.github.FieryCod/holy-lambda   "0.1.50"
+(defproject io.github.FieryCod/holy-lambda   "0.1.51-SNAPSHOT"
   :description "Micro framework which turns your code into AWS Lambda functions"
 
   :url "https://github.com/FieryCod/holy-lambda"
