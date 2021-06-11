ce054af60c9344acf29b36fb71c153be32e8f0a8
Karol Wójcik <karol.wojcik@tuta.io>
2021-06-11 15:02:54 +0200

    Fix spit to file which does not exists

--- modules/holy-lambda-babashka-tasks/src/holy_lambda/tasks.clj
+++ modules/holy-lambda-babashka-tasks/src/holy_lambda/tasks.clj
@@ -10,7 +10,8 @@
    [babashka.fs :as fs]
    [babashka.curl :as curl]
    [babashka.process :as p]
-   [clojure.java.io :as io]))
+   [clojure.java.io :as io])
+  (:refer-clojure :exclude [spit]))
 
 (deps/add-deps {:deps {'clojure-term-colors/clojure-term-colors {:mvn/version "0.1.0"}}})
 
@@ -120,6 +121,11 @@
                    (= (:exit (csh/sh "pgrep" "-f" "Docker.app")) 0)))
   (hpr (pre "Docker is not running! Enable and run docker first before using holy-lambda!"))
   (System/exit 1))
+
+(defn spit
+  [file content]
+  (io/make-parents file)
+  (clojure.core/spit file content))
 
 (def AVAILABLE_RUNTIMES #{:babashka :native :java})
 (def AVAILABLE_REGIONS #{"us-east-2", "us-east-1", "us-west-1", "us-west-2", "af-south-1", "ap-east-1", "ap-south-1", "ap-northeast-3", "ap-northeast-2", "ap-southeast-1", "ap-southeast-2", "ap-northeast-1", "ca-central-1", "cn-north-1", "cn-northwest-1", "eu-central-1", "eu-west-1", "eu-west-2", "eu-south-1", "eu-west-3", "eu-north-1", "me-south-1", "sa-east-1"})
