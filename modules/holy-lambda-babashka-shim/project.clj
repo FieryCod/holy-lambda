50c055b1b9addf6bfc25bca8a1deecfd3af5c36f
Karol Wójcik <karol.wojcik@tuta.io>
2021-06-15 10:55:50 +0200

    Bump babashka version 0.4.6

--- modules/holy-lambda-babashka-shim/project.clj
+++ modules/holy-lambda-babashka-shim/project.clj
@@ -1,4 +1,4 @@
-(defproject io.github.FieryCod/holy-lambda-babashka-shim "0.1.54"
+(defproject io.github.FieryCod/holy-lambda-babashka-shim "0.2.1"
   :description "Micro framework which turns your code into AWS Lambda functions suites for babashka based Lambdas"
 
   :url "https://github.com/FieryCod/holy-lambda/tree/master/modules/holy-lambda-babashka"
