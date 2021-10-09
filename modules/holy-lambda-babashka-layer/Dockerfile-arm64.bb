5a795d99125d86f0f9d788807e5017ee17c2bce0
Karol Wójcik <karol.wojcik@tuta.io>
2021-10-09 22:33:04 +0200

    Publish new HL babashka layer

---
+++ modules/holy-lambda-babashka-layer/Dockerfile-arm64.bb
@@ -0,0 +1,27 @@
+FROM clojure:tools-deps as BUILDER
+
+WORKDIR /opt
+
+RUN apt-get update && apt-get install -y wget zip
+
+ENV GITLIBS=".gitlibs/"
+ENV CLOJURE_TOOLS_DIR=/opt
+ARG BABASHKA_VERSION=0.6.2
+
+COPY bootstrap .
+COPY deps.edn .
+COPY hacks.clj .
+
+RUN rm -Rf /opt/.m2
+
+# Setup path
+RUN clojure -Sdeps '{:mvn/local-repo "/opt/.m2"}' -Spath
+
+# Setup babashka
+RUN wget -c https://github.com/babashka/babashka/releases/download/v$BABASHKA_VERSION/babashka-$BABASHKA_VERSION-linux-aarch64.tar.gz -O - | tar -xz && chmod +x bb
+
+# # Setup deps.tools
+# RUN curl https://raw.githubusercontent.com/borkdude/deps.clj/master/install | bash && deps -P
+
+# Zip all deps together. Resources should be distributed as layers
+RUN zip -q -r holy-lambda-babashka-runtime-arm64.zip bb bootstrap .m2 hacks.clj
---
+++ modules/holy-lambda-babashka-layer/publish.clj
@@ -0,0 +1,54 @@
+(require '[selmer.parser :as selm])
+(require '[babashka.process :as p])
+(require '[clojure.string :as s])
+
+(def ARM64 "arm64")
+(def AMD64 "amd64")
+
+(def ARCHS #{ARM64 AMD64})
+(def FILES #{"README.md.template" "Dockerfile.bb.template" "template.yml.template"})
+
+(defn arch->subcord
+  [arch]
+  (get {"arm64" "aarch64" "amd64" "amd64"} arch))
+
+(defn template->filename
+  [template arch]
+  (let [[file ext] (s/split (subs template 0 (- (count template) 9)) #"\.")]
+    (str file "-" arch "." ext)))
+
+(defn render
+  [template arch]
+  (spit (template->filename template arch) (selm/render (slurp template) {:arch arch :aarch (arch->subcord arch) :arch_is (if (= arch "arm64") "arm64" "x86_64")})))
+
+(defn- shell
+  [cmd]
+  @(p/process (p/tokenize cmd) {:inherit true}))
+
+(def task (first *command-line-args*))
+
+(when (= task "render")
+  (doseq [arch ARCHS]
+    (doseq [file FILES]
+      (render file arch))))
+
+(when (= task "build")
+  (doseq [arch ARCHS]
+    (shell (str "docker build . --target BUILDER -t holy-lambda-babashka-layer-" arch  " -f Dockerfile-" arch ".bb"))
+    (shell "bash -c \"docker rm build || true\"")
+    (shell (str "docker create --name build holy-lambda-babashka-layer-" arch))
+    (shell (str "docker cp build:/opt/holy-lambda-babashka-runtime-" arch ".zip holy-lambda-babashka-runtime-" arch ".zip"))
+    (shell "bash -c \"docker rm build || true\"")
+    (shell (str "docker image rm -f holy-lambda-babashka-layer-" arch))))
+
+(def SEMANTIC_VERSION (s/trim (slurp "LAYER_VERSION")))
+
+(when (= task "publish")
+  (doseq [arch ARCHS]
+    (let [arch arch
+          version SEMANTIC_VERSION
+          s3 "holy-lambda-babashka-layer"
+          s3-prefix "holy-lambda"
+          region "eu-central-1"]
+      (shell (selm/<< "sam package --template-file template-{{arch}}.yml --output-template-file packaged-{{arch}}.yml --s3-bucket {{s3}} --s3-prefix {{s3-prefix}}"))
+      (shell (selm/<< "sam publish --template-file packaged-{{arch}}.yml --semantic-version {{version}}")))))
